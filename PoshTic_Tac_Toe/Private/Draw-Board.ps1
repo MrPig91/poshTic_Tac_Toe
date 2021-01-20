2187e0478e4ea1de075c92c8f23f5afe12097612
MrPig91 <sirius.cleveland@gmail.com>
2021-01-19 23:33:42 -0600

    Changed score number color

--- PoshTic_Tac_Toe/Private/Draw-Board.ps1
+++ PoshTic_Tac_Toe/Private/Draw-Board.ps1
@@ -15,8 +15,8 @@ function Draw-Board {
     $topborder   $topborder
     $([char]9474)  $(Write-tttColorText -Text $gameGrid[0].Value) $([char]9474) $(Write-tttColorText -Text $gameGrid[1].Value) $([char]9474) $(Write-tttColorText -Text $gameGrid[2].Value)  $([char]9474)   $([char]9474)    SCORE    $([char]9474)
     $([char]9474) $middleborder $([char]9474)   $([char]9500)$line$([char]9508) 
-    $([char]9474)  $(Write-tttColorText -Text $gameGrid[3].Value) $([char]9474) $(Write-tttColorText -Text $gameGrid[4].Value) $([char]9474) $(Write-tttColorText -Text $gameGrid[5].Value)  $([char]9474)   $([char]9474) $(Write-tttColorText -Text "Player X: $($PlayerXScore.ToString("0"))" -Color "Green") $([char]9474)  
-    $([char]9474) $middleborder $([char]9474)   $([char]9474) $(Write-tttColorText -Text $("Player O: $($PlayerOScore.ToString("0"))") -Color "Red") $([char]9474)  
+    $([char]9474)  $(Write-tttColorText -Text $gameGrid[3].Value) $([char]9474) $(Write-tttColorText -Text $gameGrid[4].Value) $([char]9474) $(Write-tttColorText -Text $gameGrid[5].Value)  $([char]9474)   $([char]9474) $(Write-tttColorText -Text "Player X: " -Color "Green")$($PlayerXScore.ToString("0")) $([char]9474)  
+    $([char]9474) $middleborder $([char]9474)   $([char]9474) $(Write-tttColorText -Text $("Player O: ") -Color "Red")$($PlayerOScore.ToString("0")) $([char]9474)  
     $([char]9474)  $(Write-tttColorText -Text $gameGrid[6].Value) $([char]9474) $(Write-tttColorText -Text $gameGrid[7].Value) $([char]9474) $(Write-tttColorText -Text $gameGrid[8].Value)  $([char]9474)   $bottomborder
     $bottomborder
         "
