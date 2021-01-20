cb8e3424c675195d564dde764393b1ac6ccefcc6
MrPig91 <sirius.cleveland@gmail.com>
2021-01-19 23:34:56 -0600

    fixed Test-DrawCondition

--- PoshTic_Tac_Toe/Private/Test-DrawCondition.ps1
+++ PoshTic_Tac_Toe/Private/Test-DrawCondition.ps1
@@ -1,10 +1,10 @@
-Test-DrawCondition {
+function Test-DrawCondition {
     param(
         $game
     )
 
   for ($x = 0; $x -le 8; $x++){
-    if ($game[$X].Value -eq $null){
+    if ($game[$X].Value -eq " "){
       return $false
     }
   }
